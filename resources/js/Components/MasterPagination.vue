2b7ebdb37214bfd1e18df1caf51cb07cabb382b7
ChanMyae74 <chanmyae1997.mm@gmail.com>
2023-12-23 22:40:00 +0630

    first-commit

---
+++ resources/js/Components/MasterPagination.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex flex-wrap justify-center gap-x-2">
+        <Link
+            v-for="link in links"
+            :class="{
+                'leading-4 flex h-8 items-center justify-center rounded-xl border px-3 py-2 text-2xs text-blue-500 dark:text-blue-300 duration-300 font-bold dark:hover:text-blue-500 bg-blue/7 border-blue/75':
+                    link.active,
+                'leading-4 flex h-8 items-center justify-center rounded-xl hover:border-blue-500 px-3 py-2 text-xs font-bold hover:text-blue-500 duration-300 dark:hover:text-blue-300 bg-blue/7':
+                    link,
+            }"
+            :href="link.url"
+        >
+            <span v-html="link.label"></span>
+        </Link>
+    </div>
+</template>
+
+<script>
+import {Link} from '@inertiajs/vue3';
+
+export default {
+    name: "MasterPagination",
+    props: {
+        links: Array,
+    },
+    components: {Link}
+};
+</script>
+
+<style>
+a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: background-color 0.3s, border 0.3s;
+}
+</style>
